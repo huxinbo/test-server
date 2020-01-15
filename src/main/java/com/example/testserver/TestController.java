763ff3e76e4ded7b72d4dea17af5ffc6fc951818
huxinbo <huxinbo56@163.com>
2020-01-15 16:49:44 +0800

    打包Jar

---
+++ src/main/java/com/example/testserver/TestController.java
@@ -0,0 +1,13 @@
+package com.example.testserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "启动成功";
+    }
+}
